bcb17d3f5a9dc0d3a5519ffb3bd7e7519d682a10
Edu Santos <eduardosantosj2@gmail.com>
2024-04-25 17:16:52 -0300

    initial commit

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+const button = document.getElementById('no-option');
+const buttonyes = document.getElementById('yes');
+
+button.addEventListener('mouseover', () => {
+    const newX = Math.random() * (window.innerWidth - button.offsetWidth);
+    const newY = Math.random() * (window.innerHeight - button.offsetHeight);
+    
+    button.style.left = newX + 'px';
+    button.style.top = newY + 'px';
+    button.style.position = 'absolute'
+});
+
+buttonyes.addEventListener('click', () => {
+    // Redirecionar para a página 2
+    window.location.href = 'pagina2.html'; // Altere 'pagina2.html' para o caminho da sua segunda página
+});
