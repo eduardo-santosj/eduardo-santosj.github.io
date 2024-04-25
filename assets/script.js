e773d18d88f86edcba05e17341ad6d3c21d655f9
Edu Santos <eduardosantosj2@gmail.com>
2024-04-25 17:30:36 -0300

    initial commit

--- assets/script.js
+++ assets/script.js
@@ -10,6 +10,15 @@ button.addEventListener('mouseover', () => {
     button.style.position = 'absolute'
 });
 
+button.addEventListener('touchstart', () => {
+    const newX = Math.random() * (window.innerWidth - button.offsetWidth);
+    const newY = Math.random() * (window.innerHeight - button.offsetHeight);
+    
+    button.style.left = newX + 'px';
+    button.style.top = newY + 'px';
+    button.style.position = 'absolute'
+});
+
 buttonyes.addEventListener('click', () => {
     // Redirecionar para a página 2
     window.location.href = 'pagina2.html'; // Altere 'pagina2.html' para o caminho da sua segunda página
